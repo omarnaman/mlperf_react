9617c3fc419c7480528468f841be42c5841add3c
mario <sumario82@gmail.com>
2022-01-31 16:27:26 -0500

    client config2

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { Parameter, StartButton, Title } from './components';
+
 const App = () => {
   return (
   <div>
---
+++ client/src/components/index.js
@@ -0,0 +1,3 @@
+export { default as Title } from './title/Title';
+export { default as Parameter } from './parameter/Parameter';
+export { default as StartButton } from './startButton/StartButton';
