758eb0f11c2172cc8848cf7940468ddb11a2e35b
mario <sumario82@gmail.com>
2022-01-31 22:33:01 -0500

    fetching works, startjob works

--- client/src/components/startButton/StartButton.jsx
+++ client/src/components/startButton/StartButton.jsx
@@ -7,17 +7,17 @@ class StartButton extends React.Component {
       this.state = {id:"sdfa12", timeMS:20000, clients:4};
   }
 
-  async componentDidMount() {
-    fetch('http://localhost:8081/StartJob/4/4')
+  handleStart() {
+    fetch('StartJob/5/5')
       .then(res => res.json())
-      .then(data => this.setState(data), () => console.log(this.state.id))
+      .then(data => console.log(data), () => console.log("job started"))
   }
-
-  /*componentDidMount(){
-    fetch('/test')
+  
+  componentDidMount(){
+    fetch('test')
       .then(res => res.json())
       .then(data => console.log(data), () => console.log("test fetched"))
-  }*/
+  }
 
   render() {
     return (
