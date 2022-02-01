afa90b141e0034c50aee12214bb04fe6d92e9464
mario <sumario82@gmail.com>
2022-01-31 22:22:28 -0500

    fetching wrong port

--- client/src/components/startButton/StartButton.jsx
+++ client/src/components/startButton/StartButton.jsx
@@ -3,26 +3,21 @@ import './startButton.css';
 
 class StartButton extends React.Component {
   constructor() {
-    super();
-    this.state = {
-      id:"sdfas", 
-      timeMS:20000, 
-      clients:2
-    };
+      super();
+      this.state = {id:"sdfa12", timeMS:20000, clients:4};
   }
 
-  handleStart() {
-    console.log("button clicked");
-    fetch('/StartJob/4/10')
-            .then(res => res.json())
-            .then(data => this.setState(data), () => console.log(this.state.id))
+  async componentDidMount() {
+    fetch('http://localhost:8081/StartJob/4/4')
+      .then(res => res.json())
+      .then(data => this.setState(data), () => console.log(this.state.id))
   }
 
-  componentDidMount(){
+  /*componentDidMount(){
     fetch('/test')
       .then(res => res.json())
       .then(data => console.log(data), () => console.log("test fetched"))
-  }
+  }*/
 
   render() {
     return (
