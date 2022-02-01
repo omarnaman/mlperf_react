411a0d8b48c13da938096435c4d87dd8078acc73
mario <sumario82@gmail.com>
2022-01-31 21:38:29 -0500

    server running

--- client/src/components/startButton/StartButton.jsx
+++ client/src/components/startButton/StartButton.jsx
@@ -2,10 +2,23 @@ import React from 'react';
 import './startButton.css';
 
 class StartButton extends React.Component {
+  constructor() {
+    super();
+    this.state = {id:"sdfas", timeMS:20000, clients:2};
+  }
+
+  handleStart() {
+    console.log("button clicked");
+    fetch("/StartJob/4/10")
+    .then(res => res.json())
+    .then(data => this.setState(data), () => console.log(this.state.id))
+  }
+
+
   render() {
     return (
       <div>
-        <p className='StartButton'>Begin Experiment</p>
+        <button onClick={this.handleStart} className='StartButton'>Begin Experiment</button>
       </div>
     )
   }
