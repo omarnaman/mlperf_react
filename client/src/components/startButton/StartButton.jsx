bed9baafca736c3311af34c738e223e5ae201679
mario <sumario82@gmail.com>
2022-01-31 23:14:43 -0500

    really bad leading icons

--- client/src/components/startButton/StartButton.jsx
+++ client/src/components/startButton/StartButton.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './startButton.css';
+import pause from './pause-button.png';
+import load from './loading2.png';
 
 class StartButton extends React.Component {
   constructor() {
@@ -23,9 +25,31 @@ class StartButton extends React.Component {
       .then(data => console.log(data), () => console.log("test fetched"))
   }*/
 
+  componentDidMount() {
+    var timeID = setInterval(() => {
+      let runningURL = "IsRunning/" + this.state.id;
+      let resp = {};
+      fetch(runningURL)
+        .then(res => res.json())
+        .then(data => {
+          if (data.isRunning){
+            document.getElementById("status").src = load
+            document.getElementById("statusDescription").innerHTML = "jobId " + this.state.id + " is running"
+          } else {
+            document.getElementById("statusDescription").innerHTML = "no job is running"
+            document.getElementById("status").src = pause
+          }
+        }, () => {            
+            document.getElementById("statusDescription").innerHTML = "no job is running"
+            document.getElementById("status").src = pause})
+    },5000);
+  } 
+
   render() {
     return (
-      <div>
+      <div className='startContain'>
+        <img id="status" className="statusIcon" src={pause}></img>
+        <p id="statusDescription"></p>
         <button onClick={this.handleStart} className='StartButton'>Begin Experiment</button>
       </div>
     )
