1e68d65ac7d83210133deafd093e5f66dacd6ac4
mario <sumario82@gmail.com>
2022-02-07 16:30:19 -0500

    router implemeneted

---
+++ client/src/pages/results/Results.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./results.css";
+
+const Results = () => {
+  return (
+  <div>
+    <h1>testting results page</h1>
+  </div>
+  )
+};
+
+export default Results;
