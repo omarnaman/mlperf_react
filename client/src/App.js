6c138db12a008ec96b6802190fc65a5dd52c04ec
mario <sumario82@gmail.com>
2022-03-01 17:26:27 -0500

    tabular design

--- client/src/App.js
+++ client/src/App.js
@@ -7,11 +7,24 @@ import Results from "./pages/results/Results"
 const App = () => {
   return (
     <Router>
-      {/* anything here (outside the "ROUTES" tag) will show up on all pages */}
-      <Routes>
-        <Route path="/" element={<Config />}></Route>
-        <Route path="/Results" element={<Results />}></Route>
-      </Routes>
+      <div className='header'>
+        <p>Model Version: </p>
+        <p>Tool Name: </p>
+        <p>Tool Version: </p>
+      </div>
+      <div className='main'>
+        <div className='pageBar'>
+          <p className='pageTab'>Configuration</p>
+          <p className='pageTab'>Results</p>
+          <p className='pageTab'>Settings</p>
+        </div>
+        <div className='display'>
+          <Routes>
+            <Route path="/" element={<Config />}></Route>
+            <Route path="/Results" element={<Results />}></Route>
+          </Routes>
+        </div>
+      </div>
     </Router>
   )
 };
