a72f6881435881b365c4a0d7f67876d44e022760
mario <sumario82@gmail.com>
2022-03-03 00:12:53 -0500

    midddletab2

--- client/src/containers/loadGenerator/LoadGenerator.jsx
+++ client/src/containers/loadGenerator/LoadGenerator.jsx
@@ -5,7 +5,40 @@ import { Title, Parameter, ImportButton } from '../../components';
 const LoadGenerator = () => {
   return (
   <div className='genConfig'>
-    <Title name="Load Generatator Configuration"/>
+    <Title name="Load Generator Configuration"/>
+    <div className='paramContain'>
+      <div className='paramTogether'>
+        <Parameter title="DataSet" num=""/>
+        <select className='dropdown'>
+          <option>coco</option>
+          <option>imagenet2012</option>
+        </select>
+      </div>
+      <div className='paramTogether'>
+        <Parameter title="Scenario" num=""/>
+        <select className='dropdown'>
+          <option>singleStream</option>
+          <option>multiStream</option>
+          <option>offline</option>
+        </select>
+      </div>
+      <div className='paramTogether'>
+        <Parameter title="Count" num=""/>
+        <input placeholder=" num" className="numBox" type="text"></input>
+      </div>
+      <div className='paramTogether'>
+        <Parameter title="Threads" num=""/>
+        <input placeholder=" num" className="numBox" type="text"></input>
+      </div>
+      <div className='paramTogether'>
+        <Parameter title="Max-Batchsize" num=""/>
+        <input placeholder=" num" className="numBox" type="text"></input>
+      </div>
+      <div className='paramTogether'>
+        <Parameter title="Record Accuracy" num=""/>
+        <input defaultChecked className="checkBox" type="checkbox"></input>
+      </div>
+    </div>
     <ImportButton />
   </div>
   )
