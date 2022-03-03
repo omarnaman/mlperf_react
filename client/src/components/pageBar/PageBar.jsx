469bee2d0bff794796acf76b7b9d16bfa7088f35
mario <sumario82@gmail.com>
2022-03-03 03:44:12 -0500

    simple switches working

--- client/src/components/pageBar/PageBar.jsx
+++ client/src/components/pageBar/PageBar.jsx
@@ -5,13 +5,17 @@ import './pageBar.css';
 function PageBar() {
   let navigate = useNavigate();
 
+  let handleConfigTab = () => {
+    navigate("");
+  }
+
   let handleResultsTab = () => {
     navigate("/results");
   }
 
   return (
     <div className='pageBar'>
-      <p className='pageTab'>Configuration</p>
+      <p onClick={handleConfigTab} className='pageTab'>Configuration</p>
       <p onClick={handleResultsTab} className='pageTab2'>Results</p>
       <p className='pageTab3'>Settings</p>
     </div>
