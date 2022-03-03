a4bee1abf756194289b0329ff34d79c045a7c971
mario <sumario82@gmail.com>
2022-03-03 03:41:55 -0500

    tabswitch

--- client/src/containers/configTabBar/ConfigTabBar.jsx
+++ client/src/containers/configTabBar/ConfigTabBar.jsx
@@ -1,20 +1,40 @@
 import React from 'react';
 import "./configTabBar.css"
-import LoadGenerator, { loadGenerator } from '.././loadGenerator/LoadGenerator'
+import LoadGenerator from '../configTabs/loadGenerator/LoadGenerator'
+import SystemTest from '../configTabs/systemTest/SystemTest';
 
 function ConfigTabBar() {
+
+  let handleLoad = () => {
+    document.getElementById('loadGeneratorTab').style.display = "flex";
+    document.getElementById('configTab').style.borderRight = "0vh solid black;";
+    document.getElementById('systemTestTab').style.display = "none";
+    document.getElementById('configTab2').style.borderRight = "0.3vh solid black;";
+  }
+
+  let handleSystemTest = () => {
+    document.getElementById('loadGeneratorTab').style.display = "none";
+    document.getElementById('configTab').style.borderRight = "0.3vh solid black;";
+    document.getElementById('systemTestTab').style.display = "flex";
+    document.getElementById('configTab2').style.borderRight = "0vh solid black;";
+  }
+
   return (
     <div className='configFull'>
       <div className='configTabBar'>
-        <p className='configTab'>Load Generator</p>
-        <p className='configTab2'>System Under Test</p>
-        <p className='configTab3'>Network Emulation</p>
-        <p className='configTab4'>Profiles</p>
-        <p className='configTab5'>Cloud Development</p>
+        <p onClick={handleLoad} id='configTab'>Load Generator</p>
+        <p onClick={handleSystemTest} id='configTab2'>System Under Test</p>
+        <p id='configTab3'>Network Emulation</p>
+        <p id='configTab4'>Profiles</p>
+        <p id='configTab5'>Cloud Development</p>
       </div>
-      <div className='loadgenContain'>
-        {/* put each config page tab here */}
-        <LoadGenerator/>
+      <div className='configTabContain'>
+        <div id='loadGeneratorTab'>
+          <LoadGenerator/>
+        </div>
+        <div id='systemTestTab'>
+          <SystemTest/>
+        </div>
 
       </div>
     </div>
--- client/src/containers/configTabs/loadGenerator/LoadGenerator.jsx
+++ client/src/containers/configTabs/loadGenerator/LoadGenerator.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import "./loadGenerator.css";
-import { Title, Parameter, ImportButton } from '../../components';
+import { Title, Parameter, ImportButton } from '../../../components';
 
 const LoadGenerator = () => {
   return (
@@ -31,11 +31,27 @@ const LoadGenerator = () => {
         <input placeholder=" num" className="numBox" type="text"></input>
       </div>
       <div className='paramTogether'>
-        <Parameter title="Max-Batchsize" num=""/>
+        <Parameter title="Max Batchsize" num=""/>
         <input placeholder=" num" className="numBox" type="text"></input>
       </div>
       <div className='paramTogether'>
         <Parameter title="Record Accuracy" num=""/>
+        <input defaultChecked className="checkBox" type="checkbox"></input>
+      </div>
+      <div className='paramTogether'>
+        <Parameter title="Time" num=""/>
+        <input placeholder=" text"className="TextBox" type="text"></input>
+      </div>
+      <div className='paramTogether'>
+        <Parameter title="Samples Per Query" num=""/>
+        <input placeholder=" text"className="TextBox" type="text"></input>
+      </div>
+      <div className='paramTogether'>
+        <Parameter title="Pipeline" num=""/>
+        <input defaultChecked className="checkBox" type="checkbox"></input>
+      </div>
+      <div className='paramTogether'>
+        <Parameter title="Max Outgoing" num=""/>
         <input defaultChecked className="checkBox" type="checkbox"></input>
       </div>
     </div>
