5b41af54522742dc6a0622c4056bc9210f6a93c7
Omar Naman <onaman@uwaterloo.ca>
2022-05-13 22:12:02 -0400

    Fix a bug causing the produced YAML to contain errors

--- flask_backend/yaml_builder.py
+++ flask_backend/yaml_builder.py
@@ -6,6 +6,16 @@ class Metadata:
         self.labels = {
             "name": label
         }
+    def to_dict(self):
+        d = {
+            "name": self.name,
+            "labels": self.labels
+        }
+        keys = list(d.keys())
+        for key in keys:
+            if d[key] is None:
+                del d[key]
+        return d
 
 class Container:
     def __init__(self, image, name, args=None, imagePullPolicy=None, command=None):
@@ -15,6 +25,19 @@ class Container:
         self.imagePullPolicy = imagePullPolicy
         self.command = command
     
+    def to_dict(self):
+        d = {
+            "image": self.image,
+            "name": self.name,
+            "args": self.args,
+            "imagePullPolicy": self.imagePullPolicy,
+            "command": self.command,
+        }
+        keys = list(d.keys())
+        for key in keys:
+            if d[key] is None:
+                del d[key]
+        return d
 
 
 class JobSpec:
@@ -26,19 +49,31 @@ class JobSpec:
             Container("busybox", "wait-sut", None, None, ["sh", "-c", "wget sut:8086 2>&1 | grep refused; while [[ $? == 0 ]]; do sleep 2; wget sut:8086 2>&1 | grep refused; done;"])]
         self.restartPolicy= "Never"
     
+    def to_dict(self):
+        d = {
+            "conatiners": [container.to_dict() for container in self.containers],
+            "initContainers": [container.to_dict() for container in self.initContainers],
+            "restartPolicy": self.restartPolicy
+        }
+        keys = list(d.keys())
+        for key in keys:
+            if d[key] is None:
+                del d[key]
+        return d
 
 
 # eid and selector accept strings, args accepts an array of strings
 def createJobYAML(eid, selector, args) -> str:
-    args_full = [eid, selector].extend(args)
+    args_full = [eid, selector]
+    args_full.extend(args)
     o = {
         "apiVersion":"batch/v1",
         "kind": "Job",
-        "metadata": Metadata(selector, "lg"),
+        "metadata": Metadata(selector, "lg").to_dict(),
         "spec": {
             "ttlSecondsAfterFinished": 10,
             "template": {
-                "spec": JobSpec(args_full)
+                "spec": JobSpec(args_full).to_dict()
             }
         }
     }
