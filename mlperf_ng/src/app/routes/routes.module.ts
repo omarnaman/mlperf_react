d781ccbb0dbf0a57557a7d3fad16fe19330ad2b6
Hala <hqadi@uwaterloo.ca>
2023-03-05 21:21:01 -0500

    Added separate chart component

--- mlperf_ng/src/app/routes/routes.module.ts
+++ mlperf_ng/src/app/routes/routes.module.ts
@@ -5,7 +5,6 @@ import { RoutesRoutingModule } from './routes-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './sessions/login/login.component';
 import { RegisterComponent } from './sessions/register/register.component';
-import { NgApexchartsModule } from 'ng-apexcharts';
 import { LoadGeneratorFormComponent } from './load-generator-form/load-generator-form.component';
 import { SystemUnderTestFormComponent } from './system-under-test-form/system-under-test-form.component';
 import { NetworkEmulationFormComponent } from './network-emulation-form/network-emulation-form.component';
@@ -29,7 +28,7 @@ const COMPONENTS: any[] = [
 const COMPONENTS_DYNAMIC: any[] = [];
 
 @NgModule({
-    imports: [SharedModule, RoutesRoutingModule, NgApexchartsModule],
+    imports: [SharedModule, RoutesRoutingModule],
     declarations: [...COMPONENTS, ...COMPONENTS_DYNAMIC, RunExperimentComponent, AddProfileFormComponent, ProfileListComponent, ProfileCardComponent, ExperimentResultsComponent],
 })
 export class RoutesModule {}
